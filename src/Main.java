bd581dc1d5377839858dabe650dfdbea9ebad142
Timur Kiselev <snygens@mail.ru>
2020-06-13 12:50:20 +0300

    add project and report

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main (String[] args)
+    {
+        long balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        long amount = balance + transfer;
+        System.out.println(amount);
+    }
+}
